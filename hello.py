f6ded0f64130a9fdbae4a471c2d14cab0fcc947a
maksN7 <nezdurmaksim@gmail.com>
2024-10-28 20:23:20 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+Hello = "Hello World"
+name = "Maksym"
+lastname = "Nezdiur"
+age = 16
+
+print (Hello)
+print(name)
+print(lastname)
+print(age)
+
+print(type(Hello))
+print(type(name))
+print(type(lastname))
+print(type(age))
